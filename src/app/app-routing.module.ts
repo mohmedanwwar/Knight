804e33f8178f48518503dde4a9b098bb87cca1aa
mohamed anwar <mohamedanwarfahmy@gmail.com>
2022-11-25 13:59:18 +0200

    First project using framework angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { FaqComponent } from './main/faq/faq.component';
+import { PricingComponent } from './main/pricing/pricing.component';
+import { TeamComponent } from './main/team/team.component';
+import { ClientsComponent } from './main/clients/clients.component';
+import { TestimonialsComponent } from './main/testimonials/testimonials.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { WhyUsComponent } from './main/why-us/why-us.component';
+import { FeaturedComponent } from './main/featured/featured.component';
+import { ServicesComponent } from './main/services/services.component';
+import { AboutComponent } from './main/about/about.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"about",component : AboutComponent},
+  {path:"services",component : ServicesComponent},
+  {path:"portfolio",component : PortfolioComponent},
+  {path:"team",component : TeamComponent},
+  {path:"pricing",component : PricingComponent},
+  {path:"contact",component : ContactComponent},
+  {path:"notfound",component : NotfoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './main/about/about.component';
+import { ServicesComponent } from './main/services/services.component';
+import { FeaturedComponent } from './main/featured/featured.component';
+import { WhyUsComponent } from './main/why-us/why-us.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { TestimonialsComponent } from './main/testimonials/testimonials.component';
+import { ClientsComponent } from './main/clients/clients.component';
+import { TeamComponent } from './main/team/team.component';
+import { PricingComponent } from './main/pricing/pricing.component';
+import { FaqComponent } from './main/faq/faq.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    ServicesComponent,
+    FeaturedComponent,
+    WhyUsComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    ClientsComponent,
+    TeamComponent,
+    PricingComponent,
+    FaqComponent,
+    ContactComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
